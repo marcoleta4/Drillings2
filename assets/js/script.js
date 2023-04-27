fc69cc746596be5ec6d66f16dca56a114d233f5c
Mille Marcoleta <marcoleta.mille@gmail.com>
2023-04-27 19:19:00 -0400

    DrillinfinalS2

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var url = "https://digimon-api.vercel.app/api/digimon"
+
+var contenido = document.querySelector("#contenido")
+
+fetch(url)
+.then(response => response.json())
+.then(datos => {
+    console.log(datos)
+
+    for (item of datos){
+
+    contenido.innerHTML += `
+    <div class="tarjeta">
+        <div class="card" style="width: 18rem;">
+        <img src="${item.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">${item.level}</p>
+          <a href="${item.img}" class="btn btn-primary">eligéme</a>
+        </div>
+        </div>
+    </div>    
+    `
+
+}
+
+})
+
+const miAudio = document.getElementById('miAudio');
+const miBoton = document.querySelector('button');
+
+
+  
+  
