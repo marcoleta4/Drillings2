b1accf4ba865537b3282b9d68c9d89960ec08133
Mille Marcoleta <marcoleta.mille@gmail.com>
2023-05-01 19:13:22 -0400

    cursordigimon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var url = "https://digimon-api.vercel.app/api/digimon"
 
-var contenido = document.querySelector("#contenido")
+var contenido = document.querySelector("#contenido") 
 
 fetch(url)
 .then(response => response.json())
@@ -12,11 +12,11 @@ fetch(url)
     contenido.innerHTML += `
     <div class="tarjeta">
         <div class="card" style="width: 18rem;">
-        <img src="${item.img}" class="card-img-top" alt="...">
+        <img  src="${item.img}" class="card-img-top" alt="..." img src="./assets/img/cursor3.cur" style="cursor: url(./assets/img/cursor3.cur), auto;">
         <div class="card-body">
           <h5 class="card-title">${item.name}</h5>
           <p class="card-text">${item.level}</p>
-          <a href="${item.img}" class="btn btn-primary">eligéme</a>
+          <a "href="${item.img}" class="btn btn-primary" img src="./assets/img/cursor3.cur" style="cursor: url(./assets/img/cursor3.cur), auto;">>eligéme</a>
         </div>
         </div>
     </div>    
@@ -25,6 +25,7 @@ fetch(url)
 }
 
 })
+
 
 const miAudio = document.getElementById('miAudio');
 const miBoton = document.querySelector('button');
